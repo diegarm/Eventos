28b3e98c02261b2513e89f4afd625e6a93dcc14a
Diego Armando <dgo.npp@gmail.com>
2021-07-23 18:40:31 -0300

    eventos

---
+++ ProEventos.Api/Controllers/EventoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            
+        }
+    }
+}
---
+++ ProEventos.Api/Models/Evento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Api.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int Quantidade { get; set; }
+        public string Lote { get; set; }
+
+        public string ImagemURL { get; set; }
+    }
+}
